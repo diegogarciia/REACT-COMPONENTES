d7bfd92f17cbab1b950dcd7cb1613ae47055107b
Diego <diegogarciaangel25@gmail.com>
2026-01-10 12:40:51 +0100

    mapeo para obtener solamente los primeros 151, 1º generación

--- react-pokemon/src/actions/obtener.pokemon.action.ts
+++ react-pokemon/src/actions/obtener.pokemon.action.ts
@@ -1,18 +1,25 @@
+import { pokemonApi } from '../api/pokemon.api';
 import type { Pokemon } from '../interfaces/pokemon.interface';
-import { pokemonApi } from '../api/pokemon.api'; 
 
-export const obtenerPokemonPorNombre = async (nombre: string): Promise<Pokemon> => {
+export const obtener151Pokemon = async (): Promise<Pokemon[]> => {
     
-    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre.toLowerCase()}`);
-    const datos = await respuesta.json();
+    const respuesta = await pokemonApi.get('/pokemon?limit=151');
+    const listaBasica = respuesta.data.results;
 
-    return {
-        id: datos.id,
-        nombre: datos.name,
-        tipo: datos.types.map((t: any) => t.type.name),
-        habilidad: datos.abilities.map((a: any) => a.ability.name),
-        sprites: {
-            front: datos.sprites.front_default,
-        },
-    };
+    const promesas = listaBasica.map(async (p: any) => {
+        const detalleResp = await fetch(p.url); 
+        const datos = await detalleResp.json();
+
+        return {
+            id: datos.id,
+            nombre: datos.name,
+            tipo: datos.types.map((t: any) => t.type.name),
+            habilidad: datos.abilities.map((a: any) => a.ability.name),
+            sprites: {
+                front: datos.sprites.front_default,
+            },
+        };
+    });
+
+    return Promise.all(promesas);
 };
